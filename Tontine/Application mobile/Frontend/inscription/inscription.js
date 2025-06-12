13f3c705ee9f45e52f364812f60300bafca4a4b6
marc14444 <amanijeanmarc41@gmail.com>
2025-06-12 10:19:42 +0000

    Ajout du dossier inscription : Mariano-72

---
+++ Tontine/Application mobile/Frontend/inscription/inscription.js
@@ -0,0 +1,73 @@
+// pages/inscription/inscription.js
+Page({
+
+    pageConnexion : function(){
+            wx.redirectTo({
+              url: '/pages/connexion/connexion',
+            })
+        },
+    
+
+    /**
+     * Page initial data
+     */
+    data: {
+
+    },
+
+    /**
+     * Lifecycle function--Called when page load
+     */
+    onLoad(options) {
+
+    },
+
+    /**
+     * Lifecycle function--Called when page is initially rendered
+     */
+    onReady() {
+
+    },
+
+    /**
+     * Lifecycle function--Called when page show
+     */
+    onShow() {
+
+    },
+
+    /**
+     * Lifecycle function--Called when page hide
+     */
+    onHide() {
+
+    },
+
+    /**
+     * Lifecycle function--Called when page unload
+     */
+    onUnload() {
+
+    },
+
+    /**
+     * Page event handler function--Called when user drop down
+     */
+    onPullDownRefresh() {
+
+    },
+
+    /**
+     * Called when page reach bottom
+     */
+    onReachBottom() {
+
+    },
+
+    /**
+     * Called when user click on the top right corner to share
+     */
+    onShareAppMessage() {
+
+    }
+})
