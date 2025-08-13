d23d5bd8cf68a41400e5e0d7eb2deb42cdf742de
yoyasmine <yasmineyobouet3@gmail.com>
2025-08-13 13:21:16 +0000

    mise à jour de vinted

---
+++ Vinted/Application mobile/Frontend/miniprogram-6/pages/chargement/chargement.js
@@ -0,0 +1,71 @@
+// pages/chargement/chargement.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  autre(){
+    wx.navigateTo({
+      url: 'pages/brouillon/brouillon',
+    })
+  },
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
